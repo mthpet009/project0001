c681f5090cecac4604555b1858a0ebbfc2b5cb39
mthpet009 <65726792+mthpet009@users.noreply.github.com>
2020-07-30 11:16:29 +0200

    Add files via upload

---
+++ build/precache-manifest.054d80c14179e071be5f42d4b12577ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cd48819f5f1a177a2f61c706211aa83",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9da6a9a629f1f1f80ba9",
+    "url": "./static/css/main.da2a31dc.chunk.css"
+  },
+  {
+    "revision": "1a8827958138c85062b0",
+    "url": "./static/js/2.c3b0182c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.c3b0182c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da6a9a629f1f1f80ba9",
+    "url": "./static/js/main.b62e8cac.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
